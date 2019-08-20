97c4f07b6fa0bd102304beb0a3046fdb75258216
Craig Quiter <cquiter@gmail.com>
2019-08-20 11:05:39 -0700

    Correctly set botleague liaison host

--- common.py
+++ common.py
@@ -25,10 +25,14 @@ def add_botleague_host_watch():
     """To get realtime configuration without polling the db, we setup a
         watch on the config val to update memory of each server
         process on App Engine"""
+    global BOTLEAGUE_LIAISON_HOST
+
     db = get_config_db()
 
     # Ensure BOTLEAGUE_LIAISON_HOST is set correctly on app start
-    constants.BOTLEAGUE_LIAISON_HOST = db.get('BOTLEAGUE_LIAISON_HOST')
+    BOTLEAGUE_LIAISON_HOST = db.get('BOTLEAGUE_LIAISON_HOST')
+
+    log.info(f'Initialized botleague liaison host to {BOTLEAGUE_LIAISON_HOST}')
 
     # Create a callback on_snapshot function to capture changes
     def on_botleague_host_change(col_snapshot, changes, read_time):
