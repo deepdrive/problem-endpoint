bed98e4e8767b2f332f566dc31f7b655181649e7
Craig Quiter <cquiter@gmail.com>
2019-08-23 14:44:16 -0700

    Update constant variable names

--- common.py
+++ common.py
@@ -6,12 +6,13 @@ from loguru import logger as log
 from botleague_helpers.db import get_db
 from problem_constants import constants
 
+
 def get_jobs_db():
-    return get_db(constants.EVAL_JOBS_COLLECTION_NAME)
+    return get_db(constants.JOBS_COLLECTION_NAME)
 
 
 def get_instances_db():
-    return get_db(constants.EVAL_INSTANCES_COLLECTION_NAME)
+    return get_db(constants.WORKER_INSTANCES_COLLECTION_NAME)
 
 
 def get_config_db():
